af52aefdaf9b2a7cb6869a25c579ef26d4702f56
Anton Berezhnyi <aberezhniy@clips4sale.com>
2022-12-15 15:22:30 +0200

    Fix command errors

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 set -e
 kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --list
 
-echo -e 'Creating kafka topics']
+echo -e 'Creating kafka topics'
 kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic metaevents --replication-factor 1 --partitions 1
 kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic disciplines --replication-factor 1 --partitions 1
 kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic raw_lessons --replication-factor 1 --partitions 3
@@ -13,4 +13,4 @@ kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --to
 kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic authorized_users --replication-factor 1 --partitions 1
 
 echo -e 'Successfully created the following topics:'
-kafka-topics.sh --bootstrap-server"${KAFKA_HOST}"--list
+kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --list
