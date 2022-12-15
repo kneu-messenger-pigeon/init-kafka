4f93be76a823d63e546f71daf94f71a653c85647
Anton Berezhnyi <aberezhniy@clips4sale.com>
2022-12-15 17:43:49 +0200

    Change partions config

--- init.sh
+++ init.sh
@@ -7,9 +7,9 @@ echo -e 'Creating kafka topics'
 kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic metaevents --replication-factor 1 --partitions 1
 kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic disciplines --replication-factor 1 --partitions 1
 kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic raw_lessons --replication-factor 1 --partitions 3
-kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic raw_scores --replication-factor 1 --partitions 1
-kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic realtime_changes --replication-factor 1 --partitions 1
-kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic scores_changes_feed --replication-factor 1 --partitions 1
+kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic raw_scores --replication-factor 1 --partitions 5
+kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic realtime_changes --replication-factor 1 --partitions 5
+kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic scores_changes_feed --replication-factor 1 --partitions 5
 kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic authorized_users --replication-factor 1 --partitions 1
 
 echo -e 'Successfully created the following topics:'
