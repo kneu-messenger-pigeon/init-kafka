0cac28500ac05af4e8cc47b4d414ebc88ce01175
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-01-12 13:15:06 +0200

    Change partions count

--- init.sh
+++ init.sh
@@ -8,8 +8,7 @@ kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --to
 kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic disciplines --replication-factor 1 --partitions 1
 kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic raw_lessons --replication-factor 1 --partitions 1
 kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic raw_scores --replication-factor 1 --partitions 2
-kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic realtime_changes --replication-factor 1 --partitions 5
-kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic scores_changes_feed --replication-factor 1 --partitions 5
+kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic scores_changes_feed --replication-factor 1 --partitions 6
 kafka-topics.sh --bootstrap-server "${KAFKA_HOST}" --create --if-not-exists --topic authorized_users --replication-factor 1 --partitions 1
 
 echo -e 'Successfully created the following topics:'
